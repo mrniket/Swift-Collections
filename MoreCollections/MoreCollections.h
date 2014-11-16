4018f0729cf31198a287eeb3cd500c1655b7ba70
jbulat <jarrett.bulat@gmail.com>
2014-11-16 14:42:55 +0000

    Initial Commit

---
+++ MoreCollections/MoreCollections.h
@@ -0,0 +1,19 @@
+//
+//  MoreCollections.h
+//  MoreCollections
+//
+//  Created by Jarrett on 2014-11-16.
+//  Copyright (c) 2014 Jarrett Bulat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MoreCollections.
+FOUNDATION_EXPORT double MoreCollectionsVersionNumber;
+
+//! Project version string for MoreCollections.
+FOUNDATION_EXPORT const unsigned char MoreCollectionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoreCollections/PublicHeader.h>
+
+
